ef8ece2ae3b99d090a226eb07f5703395bb5a6c7
rupesh <rupeshspund@gmail.com>
2021-04-23 22:08:35 +0530

    added file-serve and file-upload

--- go-serve files/main.go
+++ go-serve files/main.go
@@ -2,24 +2,14 @@ package main
 
 import (
 	"fmt"
-	"html/template"
 	"net/http"
 )
 
-var tpl *template.Template
-
 func main() {
-	fmt.Println("hii")
+	fmt.Println("listening on port 8080")
 	myDir := http.Dir("./public")
 	handler := http.FileServer(myDir)
 	http.Handle("/", handler)
 
-	tpl, _ = tpl.ParseGlob("public/*.html")
-
-	http.HandleFunc("/upload", uploadFile)
-
 	http.ListenAndServe(":8080", nil)
-}
-func uploadFile(w http.ResponseWriter, r *http.Request) {
-	tpl.ExecuteTemplate(w, "index.html", nil)
 }
